0d647a38651820f249dadfefdcdfb29c55cb964b
Hugh McElderry <100percentgeneric@gmail.com>
2016-10-17 19:38:35 -0500

    Sub directory creation support
    the script can now create subdirectories for works with multiple pages,
    to avoid spamming similar images in your image folder, and help organize
    manga/doujins

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 Pixiv Fave Downloader
 =====================
 
-This script downloads your Pixiv saves to a given directory. Remembers where it left off and only
-downloads the difference. Has support for multiple users.
+This script downloads your Pixiv saves to a given directory, remembers where it left off and only
+downloads the difference. Additionally, this script also supports multiple users.
+
 
 Setup
 -----
@@ -10,6 +11,10 @@ Setup
     pip install pixivpy
 
 Modify config.json with your username, password, and desired save directory.
+subdir_threshold (Default: 0) determines how many pages a work must have before it creates a subdirectory for that work. 
+A value of 0 will disable this feature
+The directory naming structure for created subdirectories is: "artist name - artist's profile ID"
+
 
 Run
 ---
--- pixiv-fave-dl.py
+++ pixiv-fave-dl.py
@@ -18,15 +18,19 @@ for user in range(len(config["username"])):
 
   json_result = api.me_favorite_works()
   my_ids = json_result.response
-
   for my_id in my_ids:
     if (my_id.work.favorite_id <= last_fave_id):
-
       break
     url = my_id.work.image_urls['large']
     for p in range(my_id.work.page_count):
       page_url = url[:-5] + str(p) + url[-4:]
-      api.download(page_url, path=download_path+os.path.basename(page_url))
+      if (my_id.work.page_count >= config['subdir_threshold']) and (config['subdir_threshold'] != 0) : 
+        subdir_path = download_path + os.sep + my_id.work.user.name + " - " + str(my_id.work.id)
+        if not os.path.exists(subdir_path):
+          os.makedirs(subdir_path)
+      else:
+        subdir_path = download_path 
+      api.download(page_url, path= subdir_path + os.sep + os.path.basename(page_url))
       print('Downloaded [ Fave ID: ' + str(my_id.work.favorite_id) + ' ID: ' + str(my_id.work.id) + ' Page: ' + str(p) + ' ] for user ' + config['username'][user])
   config['last_fave_id'][user] = str(my_ids[0].work.favorite_id)
 
