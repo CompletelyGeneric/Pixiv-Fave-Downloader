2adbc9e0c8f596cd53c4d202b2a718bac450b0ba
Hugh McElderry <hugh@hugh.moe>
2017-06-01 10:39:16 -0400

    Update script for pixivpy3 changes, and remove subdir_threshold functionality

--- README.md
+++ README.md
@@ -11,9 +11,7 @@ Setup
     pip install pixivpy
 
 Modify config.json with your username, password, and desired save directory.  
-subdir_threshold (Default: 0) determines how many pages a work must have before it creates a subdirectory for that work.  
-Each user can set this value independantly with a value of 0 disabling this feature    
-The directory naming structure for created subdirectories is: "artist name - artist's profile ID"  
+ 
 
 
 Run
--- pixiv-fave-dl.py
+++ pixiv-fave-dl.py
@@ -6,35 +6,31 @@ import os
 from datetime import datetime
 
 api = PixivAPI()
-with open('config.json', 'r') as f: #config file location
-  config = json.load(f)
+with open('config.json', 'r') as f:  # config file location
+    config = json.load(f)
 
 download_path = config['download_path']
 
 
-
 for user in range(len(config["username"])):
-  api.login(config['username'][user], config['password'][user])
-  last_fave_id = int(config['last_fave_id'][user])
+    api.login(config['username'][user], config['password'][user])
+    last_fave_id = int(config['last_fave_id'][user])
 
-  json_result = api.me_favorite_works()
-  my_ids = json_result.response
-  for my_id in my_ids:
-    if (my_id.work.favorite_id <= last_fave_id):
-      break
-    url = my_id.work.image_urls['large']
-    for p in range(my_id.work.page_count):
-      page_url = url[:-5] + str(p) + url[-4:]
-      if (my_id.work.page_count >= config['subdir_threshold'][user]) and (config['subdir_threshold'][user] != 0) : 
-        subdir_path = download_path + os.sep + my_id.work.user.name + " - " + str(my_id.work.id)
-        if not os.path.exists(subdir_path):
-          os.makedirs(subdir_path)
-      else:
-        subdir_path = download_path 
-      api.download(page_url, path=subdir_path + os.sep + os.path.basename(page_url))
-      print('[' + datetime.now().strftime( '%d/%m/%Y %H:%M:%S') + '] \033[1;32;40m::\033[0;37;40m ' + 'Downloaded [ Fave ID: ' + str(my_id.work.favorite_id) + ' ID: ' + str(my_id.work.id) + ' Page: ' + str(p) + ' ] for user ' + config['username'][user])
-  config['last_fave_id'][user] = str(my_ids[0].work.favorite_id)
+    json_result = api.me_favorite_works()
+    my_ids = json_result.response
+    for my_id in my_ids:
+        if (my_id.work.favorite_id <= last_fave_id):
+            break
+        url = my_id.work.image_urls['large']
+        for p in range(my_id.work.page_count):
+            page_url = url[:-5] + str(p) + url[-4:]
+            api.download(page_url, path=download_path)
+            print('[' + datetime.now().strftime('%d/%m/%Y %H:%M:%S') + '] \033[1;32;40m::\033[0;37;40m ' + 'Downloaded [ Fave ID: ' +
+                  str(my_id.work.favorite_id) + ' ID: ' + str(my_id.work.id) + ' Page: ' + str(p) + ' ] for user ' + config['username'][user])
 
-print('[' + datetime.now().strftime( '%d/%m/%Y %H:%M:%S') + '] \033[1;32;40m::\033[0;37;40m ' + 'End of new faves')  
+    config['last_fave_id'][user] = str(my_ids[0].work.favorite_id)
+
+print('[' + datetime.now().strftime('%d/%m/%Y %H:%M:%S') +
+      '] \033[1;32;40m::\033[0;37;40m ' + 'End of new faves')
 with open('config.json', 'w') as f:
-  json.dump(config, f)
+    json.dump(config, f)
